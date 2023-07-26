8a4d5766c987746aaf10e1e901683881373dd649
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-07-26 17:18:26 -0400

    done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,12 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+
+    @line_items = LineItem.where(order_id: params[:id])
+    product_ids = @line_items.pluck(:product_id)
+
+    # Fetch all products based on the retrieved product IDs
+    @products = Product.where(id: product_ids)
   end
 
   def create
