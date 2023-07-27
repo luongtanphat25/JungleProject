c7b6ad8f2721a97b3bdcda2a8c52df708532050c
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-07-27 11:35:09 -0400

    done about page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def show
+    @products_count = Product.all.length
+    @categories_count = Category.all.length
   end
 end
